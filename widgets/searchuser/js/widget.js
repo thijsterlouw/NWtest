483a1442a4032ed4a6fc7d22189ef5283b500d16
Thijs Terlouw <thijsterlouw@gmail.com>
2012-03-06 11:48:15 +0100

    ajax for demo

--- widgets/searchuser/js/widget.js
+++ widgets/searchuser/js/widget.js
@@ -1,24 +1,55 @@
-function loadPage(page,query) {
-    $.get('http://127.0.0.1/widget/searchuser/?page='+page+'&query='+query,
+function loadPage(url) {
+    $.get('http://127.0.0.1/widget/searchuser/'+url,
     function (htmlResult) {
-		$("#w-searchuser").replaceWith(htmlResult); 
-		$('.noJs').hide();	
+		$("#w-searchuser").replaceWith(htmlResult);	
+		pimpJS();
+		window.location.hash = url;
     }    
     );
-	window.location.hash = page;
     return false;
 }
 
-function search(query) {
-	$.post('http://127.0.0.1/widget/searchuser/?page=0&query='+query, function(data) {
-		$('#w-searchuser').replaceWith(data);
-	});
+
+function pimpPager() {
+	$("a.m-pager").attr({
+	"onClick" : function (arr) {
+                    return "loadPage(\"" + $(this).attr("href") + "\");";
+                },
+    "href" : "#"}
+    );
+};
+
+
+function pimpSearchField() {
+  $("#w-searchuser-searchform").submit(function(event) {
+
+    /* stop form from submitting normally */
+    event.preventDefault(); 
+        
+    /* get some values from elements on the page: */
+    var $form = $( this ),
+        queryval = $form.find( 'input[name="query"]' ).val(),
+        url = 'http://127.0.0.1/widget/searchuser/?query='+queryval ;
+
+    /* Send the data using post and put the results in a div */
+    $.post( url, { query: queryval },
+      function( data ) {
+          $('#w-searchuser').replaceWith(data);
+          pimpJS();
+          window.location.hash = 'query='+queryval;
+      }
+    );
+  });
+};
+
+
+/* pimp non-JS page with AJAX-style requests */
+function pimpJS() {
+    pimpPager();
+    pimpSearchField();
 }
 
+/* pimp the page after a reload */
 $(document).ready(function() {
-#	$('.noJs').hide();
-#	var hash = window.location.hash;
-#	hash = hash.replace('#','');
-#	console.log(hash);
-#	loadPage(hash);
+	pimpJS();
 });
