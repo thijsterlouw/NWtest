05ca41e6e55db5d7b3b5ea30e9247e261e8d7a62
Thijs Terlouw <thijsterlouw@gmail.com>
2012-03-06 13:14:33 +0100

    fixed 127.0.0.1

--- widgets/searchuser/js/widget.js
+++ widgets/searchuser/js/widget.js
@@ -1,5 +1,5 @@
 function loadPage(url) {
-    $.get('http://127.0.0.1/widget/searchuser/'+url,
+    $.get('/widget/searchuser/'+url,
     function (htmlResult) {
 		$("#w-searchuser").replaceWith(htmlResult);	
 		pimpJS();
@@ -29,7 +29,7 @@ function pimpSearchField() {
     /* get some values from elements on the page: */
     var $form = $( this ),
         queryval = $form.find( 'input[name="query"]' ).val(),
-        url = 'http://127.0.0.1/widget/searchuser/?query='+queryval ;
+        url = '/widget/searchuser/?query='+queryval ;
 
     /* Send the data using post and put the results in a div */
     $.post( url, { query: queryval },
